07bacc0f53a37645822702771bd253b1fcbaeeeb
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2021-12-09 19:27:51 +0100

    Temp commit

---
+++ MintPlayer.AspNetCore.ChangePassword/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MintPlayer.AspNetCore.ChangePassword.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IEndpointRouteBuilder MapChangePassword(this IEndpointRouteBuilder endpointRouteBuilder, string changePasswordUrl)
+        {
+            endpointRouteBuilder.MapGet("/.well-known/change-password", (context) =>
+            {
+                context.Response.Redirect(changePasswordUrl);
+                return Task.CompletedTask;
+            });
+            return endpointRouteBuilder;
+        }
+    }
+}
